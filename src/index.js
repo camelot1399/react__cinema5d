113d7d9bd65921228958eb2d17edfe48875793bc
camelot1399 <camelot1399@mail.ru>
2021-10-29 11:38:34 +0300

    add compositionProject.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/scss/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
