acc509e93956cd4d49413ee93b27c3f135ff9db7
Alaya95 <rozetta.95@mail.ru>
2021-11-04 16:42:00 +0300

    the component is connected

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route } from "react-router-dom";
 import Home from "./views/home";
 import Header from "./components/header";
 import Catalog from "./views/catalog";
+import Slider from "./components/slider";
 
 function App() {
   return (
     <div className="wrapper">
       <Header className="header container" />
+      <Slider />
 
       <Route path="/" component={Home} exact />
       <Route path="/catalog" component={Catalog} exact />
